b3fd29ae6fbdff05488bdc5c5f3c0c1b5401baa8
Alan Scott <alscott@redhat.com>
2021-02-03 15:45:01 -0600

    Initial Push to Revise Single Rep to iDaaS-DREAM brand

--- platform-scripts/build-solution.sh
+++
@@ -1,8 +0,0 @@
-# Change Directory to solution on local machine
-echo "iDAAS - DREAM"
-cd /Users/alscott/Development/IntelliJ/OpenSource-iDAAS/iDAAS-DREAM/
-
-/usr/local/bin/mvn clean install
-echo "Maven Build Completed"
-/usr/local/bin/mvn package
-echo "Maven Release Completed"
